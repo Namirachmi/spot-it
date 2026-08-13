99de26835774afb606b2f32fada2f1c21d277550
Namirachmi <115543353+Namirachmi@users.noreply.github.com>
2026-08-13 22:05:17 +0700

    Update cardOurPhotobooth.jsx
    update path photobooth img

--- frontend/src/Components/stackingCards/cards/cardOurPhotobooth.jsx
+++ frontend/src/Components/stackingCards/cards/cardOurPhotobooth.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { FolderWrapper } from '../stackingCards';
 
+import photoBooth from "../../Assets/img/photoBooth.png";
+
 const PhotoboothCard = ({ index }) => {
   return (
     <FolderWrapper bgColor="#FFAF00" tabText="Our Photobooth" tabPosition="left" index={index}>
       <div className="PhotoboothFlex">
         <div className='LayerGrid'>
           <div className="MockUpBox">
-            <img src="./src/Assets/img/photoBooth.png" alt="Spot It Photobooth" />
+            <img src={photoBooth} alt="Spot It Photobooth" />
           </div>
           <p className='PhotoboothContent'> 
             A complimentary photobooth installed in public spaces. Prior to access, participants complete a brief interactive misinformation challenge (1 to 2 minutes, 5 questions), after which they receive immediate feedback on their current level of awareness, along with a QR code directing them to the accompanying website.
