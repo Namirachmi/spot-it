26e4754908001f973d1f123ca8a438c2a080eec0
Namirachmi <115543353+Namirachmi@users.noreply.github.com>
2026-08-13 21:49:30 +0700

    Update header.jsx
    Update path for logoTeam img

--- frontend/src/Components/header/header.jsx
+++ frontend/src/Components/header/header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import './header.css'
 
-import logoTeam from './Assets/img/logoTeam.png';
+import logoTeam from "../../Assets/img/logoTeam.png";
 
 const Header = () => {
   return (
