42ed4be414cf47156a8b1c1b68ab2140bd15b1d0
Namirachmi <115543353+Namirachmi@users.noreply.github.com>
2026-08-13 23:06:34 +0700

    Update quizAiTemplate.jsx

--- frontend/src/Components/quiz/aiOrRealQuiz/quizAiTemplate.jsx
+++ frontend/src/Components/quiz/aiOrRealQuiz/quizAiTemplate.jsx
@@ -6,8 +6,8 @@ import Header from '../../header/header';
 import { aiQuizStages } from './aiOrRealData';
 import './quizAiTemplate.css';
 
-import starTeal from '../../Assets/img/starTeal.png';
-import starPurple from '../../Assets/img/starPurple.png';
+import starTeal from '../../../Assets/img/starTeal.png';
+import starPurple from '../../../Assets/img/starPurple.png';
 
 
 const QuizAiTemplate = () => {
